1a29b1c39ee39836de5920275ec54b59c1efe535
天火流光 <gc.thlg@gmail.com>
2021-03-28 23:28:50 +0800

    更新 hidden-logo-script
    Signed-off-by: 天火流光 <gc.thlg@gmail.com>

--- hidden-logo-script.user.js
+++ hidden-logo-script.user.js
@@ -1,24 +1,24 @@
 // ==UserScript==
 // @name         隐藏徽标
 // @namespace    https://github.com/GuoChen-thlg
-// @version      0.1.0
+// @version      0.2.0
 // @description  隐藏视频播放时出现的网站LOGO
 // @author       THLG
 // @supportURL   gc.thlg@gmail.com
 // @match        *://v.qq.com/*
 // @match        *://*.iqiyi.com/*
+// @match        *://v.youku.com/*
 // @grant        none
 // ==/UserScript==
 (function () {
     'use strict';
-    const style = `
-    
-    
-    .iqp-player [data-player-hook="logo"].iqp-logo-box,
-    [id="mod_player"] [data-role="txp-ui-watermark-mod"]{
-        display:none !important
-    }`
-    const n_style = document.createElement('style')
+    var style = [
+        "[id='mod_player'] [data-role='txp-ui-watermark-mod']",
+        ".iqp-player [data-player-hook='logo'].iqp-logo-box",
+        "#ykPlayer .youku-layer-logo",
+
+    ].join(',') + "{ display:none !important }"
+    var n_style = document.createElement('style')
     n_style.innerHTML = style
-    document.querySelector('head').appendChild(n_style)
+    document.getElementsByTagName('head')[0].appendChild(n_style)
 })();
