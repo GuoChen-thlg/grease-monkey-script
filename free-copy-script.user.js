42064c3ffd5dd75aef023dfeead22ee46e17693c
天火流光 <gc.thlg@gmail.com>
2021-03-29 15:24:12 +0800

    更新信息
    Signed-off-by: 天火流光 <gc.thlg@gmail.com>

--- free-copy-script.user.js
+++ free-copy-script.user.js
@@ -1,15 +1,20 @@
 // ==UserScript==
-// @name         Free Copy
-// @name:CN:zh   自由复制
-// @namespace    https://github.com/GuoChen-thlg
-// @version      0.1.0
-// @description  支持复制受限网站的文本内容
-// @author       THLG
-// @supportURL   gc.thlg@gmail.com
-// @match        *://wenku.baidu.com/*
-// @match        *
-// @grant        none
-// @run-at       document-end
+// @name                Free Copy
+// @name:CN:zh          自由复制
+// @namespace           https://github.com/GuoChen-thlg
+// @version             0.1.0
+// @description         支持复制受限网站的文本内容
+// @author              THLG
+// @supportURL          gc.thlg@gmail.com
+// @updateURL           https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/free-copy-script.user.js
+// @installURL          https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/free-copy-script.user.js
+// @downloadURL         https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/free-copy-script.user.js
+// @contributionURL     https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CK755FJ9PSBZ8
+// @contributionAmount  1
+// @match               *://wenku.baidu.com/*
+// @match               *
+// @grant               none
+// @run-at              document-end
 // ==/UserScript==
 (function () {
     'use strict';
--- hidden-logo-script.user.js
+++ hidden-logo-script.user.js
@@ -1,17 +1,22 @@
 // ==UserScript==
-// @name         隐藏徽标
-// @namespace    https://github.com/GuoChen-thlg
-// @version      0.2.1
-// @description  隐藏视频播放时出现的网站LOGO (支持：腾讯，爱奇艺，优酷，PP)
-// @author       THLG
-// @supportURL   gc.thlg@gmail.com
-// @match        *://v.qq.com/*
-// @match        *://*.iqiyi.com/*
-// @match        *://v.youku.com/*
-// @match        *://v.pptv.com/*
-// @match        *://*.mgtv.com/*
-// @grant        none
-// @run-at       document-end
+// @name                隐藏徽标
+// @namespace           https://github.com/GuoChen-thlg
+// @version             0.2.1
+// @description         隐藏视频播放时出现的网站LOGO (支持：腾讯，爱奇艺，优酷，PP)
+// @author              THLG
+// @supportURL          gc.thlg@gmail.com
+// @updateURL           https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/hidden-logo-script.user.js
+// @installURL          https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/hidden-logo-script.user.js
+// @downloadURL         https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/hidden-logo-script.user.js
+// @contributionURL     https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CK755FJ9PSBZ8
+// @contributionAmount  1
+// @match               *://v.qq.com/*
+// @match               *://*.iqiyi.com/*
+// @match               *://v.youku.com/*
+// @match               *://v.pptv.com/*
+// @match               *://*.mgtv.com/*
+// @grant               none
+// @run-at              document-end
 // ==/UserScript==
 (function () {
     'use strict';
