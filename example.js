cb28e92189dfda740e5f65b10e6038a4508b4318
天火流光 <gc.thlg@gmail.com>
2023-01-07 14:29:34 +0800

    ✨ feat: 添加模板文件
    Signed-off-by: 天火流光 <gc.thlg@gmail.com>

---
+++ example.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name                name
+// @namespace           https://github.com/GuoChen-thlg
+// @version             1.0.0
+// @description         简介
+// @author              THLG
+// @supportURL          gc.thlg@gmail.com
+// @updateURL           https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/
+// @installURL          https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/
+// @downloadURL         https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/
+// @contributionURL     https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CK755FJ9PSBZ8
+// @contributionAmount  1
+// @match               *://*/*
+// @grant               none
+// @run-at              document-end
+// ==/UserScript==
+(function () {
+    'use strict';
+    // code
+})();
