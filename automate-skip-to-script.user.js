958492506c9d5cccfb117a3889c2a9964a872120
天火流光 <gc.thlg@gmail.com>
2023-07-15 21:21:16 +0800

    ✨ feat: 添加自动跳转脚本

---
+++ automate-skip-to-script.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name                自动跳转
+// @namespace           https://github.com/GuoChen-thlg
+// @version             1.0.0
+// @description         自动跳转链接 懒人小工具
+// @author              THLG
+// @supportURL          gc.thlg@gmail.com
+// @updateURL           https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/automate-skip-to-script.user.js
+// @installURL          https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/automate-skip-to-script.user.js
+// @downloadURL         https://github.com/GuoChen-thlg/grease-monkey-script/raw/master/automate-skip-to-script.user.js
+// @contributionURL     https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CK755FJ9PSBZ8
+// @contributionAmount  1
+// @match               *://link.juejin.cn/*
+// @grant               none
+// @run-at              document-end
+// ==/UserScript==
+(function () {
+    'use strict';
+    // juejin.cn
+    const url = new URL(location.href)
+    if(url.host === 'link.juejin.cn'){
+        location.href = decodeURIComponent(url.searchParams.get('target'))
+    }
+    // code
+})();
