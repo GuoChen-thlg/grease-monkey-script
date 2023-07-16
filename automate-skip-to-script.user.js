5ec7b48998b0ba09f397c90788cf78e47b5c06b7
天火流光 <gc.thlg@gmail.com>
2023-07-16 17:01:59 +0800

    功能更新

--- automate-skip-to-script.user.js
+++ automate-skip-to-script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                自动跳转
 // @namespace           https://github.com/GuoChen-thlg
-// @version             1.0.0
+// @version             1.0.1
 // @description         自动跳转链接 懒人小工具
 // @author              THLG
 // @supportURL          gc.thlg@gmail.com
@@ -11,6 +11,7 @@
 // @contributionURL     https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CK755FJ9PSBZ8
 // @contributionAmount  1
 // @match               *://link.juejin.cn/*
+// @match               *://link.csdn.net/*
 // @grant               none
 // @run-at              document-end
 // ==/UserScript==
@@ -18,7 +19,7 @@
     'use strict';
     // juejin.cn
     const url = new URL(location.href)
-    if(url.host === 'link.juejin.cn'){
+    if(url.host === 'link.juejin.cn' || url.host === 'link.csdn.net'){
         location.href = decodeURIComponent(url.searchParams.get('target'))
     }
     // code
