033fde1662851308623052a942400f3705745c3c
天火流光 <gc.thlg@gmail.com>
2023-10-19 08:25:10 -0500

    快捷跳转

--- automate-skip-to-script.user.js
+++ automate-skip-to-script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                自动跳转
 // @namespace           https://github.com/GuoChen-thlg
-// @version             1.0.1
+// @version             1.0.2
 // @description         自动跳转链接 懒人小工具
 // @author              THLG
 // @supportURL          gc.thlg@gmail.com
@@ -12,6 +12,7 @@
 // @contributionAmount  1
 // @match               *://link.juejin.cn/*
 // @match               *://link.csdn.net/*
+// @match               *://gitee.com/*
 // @grant               none
 // @run-at              document-end
 // ==/UserScript==
@@ -19,7 +20,7 @@
     'use strict';
     // juejin.cn
     const url = new URL(location.href)
-    if(url.host === 'link.juejin.cn' || url.host === 'link.csdn.net'){
+    if(url.host === 'link.juejin.cn' || url.host === 'link.csdn.net' || url.host === 'gitee.com'){
         location.href = decodeURIComponent(url.searchParams.get('target'))
     }
     // code
